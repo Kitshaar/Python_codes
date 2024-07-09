c3d5b88e3add5cb91e5009b5b6daafc28463e5bb
Kartik Sharma <sharmakartik2322002@gmail.com>
2024-07-09 09:12:50 +0530

    create templates for the boilerplate pages and migrate data \n Create a superuser for admin block

--- hello/home/urls.py
+++ hello/home/urls.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
+#-----------------------
+
+admin.site.site_header = "Kitshaar Admin"
+admin.site.site_title = "Site Admin Portal"
+admin.site.index_title = "Welcome to Site Adminstrator Portal"
+
+
 # ----------------------
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("about", views.about, name="About"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
 ]
--- hello/home/views.py
+++ hello/home/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This is a home page.")
+    return render(request, "index.html")
 
 
 def about(request):
-    return HttpResponse("This is About page")
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
